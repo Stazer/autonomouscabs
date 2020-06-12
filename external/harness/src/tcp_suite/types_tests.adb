0385b63e36f7a0b7a7cdfca8fbca86c9cab3adef
Henrik Classen <henrik.classen@web.de>
2020-06-12 15:08:25 +0200

    the tests I forgot to push yesterday

--- external/harness/src/tcp_suite/types_tests.adb
+++ external/harness/src/tcp_suite/types_tests.adb
@@ -8,13 +8,20 @@ package body types_tests is
       use AUnit.Test_Cases.Registration;
    begin
       -- Repeat for each test routine:
-      Register_Routine (T, Test_Convert_Uint16'Access, "Test uint16 to Octet_2 and back.");
-      Register_Routine (T, Test_Convert_Uint32'Access, "Test uint32 to Octet_4 and back.");
-      Register_Routine (T, Test_Convert_Uint64'Access, "Test uint64 to Octet_8 and back.");
+      Register_Routine (T, Test_Uint16_To_Octet2'Access, "Test uint16 to Octet_2 and back.");
+      Register_Routine (T, Test_Uint32_To_Octet4'Access, "Test uint32 to Octet_4 and back.");
+      Register_Routine (T, Test_Uint64_To_Octet8'Access, "Test uint64 to Octet_8 and back.");
       
-      Register_Routine (T, Test_Convert_Octet2'Access, "Test Octet_2 to uint16 and back.");
-      Register_Routine (T, Test_Convert_Octet4'Access, "Test Octet_4 to uint32 and back.");
-      Register_Routine (T, Test_Convert_Octet8'Access, "Test Octet_8 to uint64 and back.");
+      Register_Routine (T, Test_Octet2_To_Uint16'Access, "Test Octet_2 to uint16 and back.");
+      Register_Routine (T, Test_Octet4_To_Uint32'Access, "Test Octet_4 to uint32 and back.");
+      Register_Routine (T, Test_Octet8_To_Uint64'Access, "Test Octet_8 to uint64 and back.");
+      
+      Register_Routine (T, Test_Uint64_To_Float64'Access, "Test uint64 to float64 and back.");
+      Register_Routine (T, Test_Float64_To_Uint64'Access, "Test float64 to uint64 and back.");
+      
+      Register_Routine (T, Test_Byte_Order_16'Access, "Test hton16 and ntoh16.");
+      Register_Routine (T, Test_Byte_Order_32'Access, "Test hton32 and ntoh32.");
+      Register_Routine (T, Test_Byte_Order_64'Access, "Test hton64 and ntoh64.");
    end Register_Tests;
 
    -- Identifier of test case
@@ -23,7 +30,7 @@ package body types_tests is
       return Format ("Types Tests");
    end Name;
 
-   procedure Test_Convert_Uint16 (T : in out Test_Cases.Test_Case'Class) is
+   procedure Test_Uint16_To_Octet2 (T : in out Test_Cases.Test_Case'Class) is
       u16, u16t : uint16;
       o2 : Octets_2;
    begin
@@ -32,9 +39,9 @@ package body types_tests is
       u16t := octets_to_uint16 (o2);
       
       Assert (u16 = u16t, "Converting uint16 to octets and back does not work");
-   end Test_Convert_Uint16;
+   end Test_Uint16_To_Octet2;
    
-   procedure Test_Convert_Uint32 (T : in out Test_Cases.Test_Case'Class) is
+   procedure Test_Uint32_To_Octet4 (T : in out Test_Cases.Test_Case'Class) is
       u32, u32t : uint32;
       o4 : Octets_4;
    begin
@@ -43,9 +50,9 @@ package body types_tests is
       u32t := octets_to_uint32 (o4);
       
       Assert (u32 = u32t, "Converting uint32 to octets and back does not work");
-   end Test_Convert_Uint32;
+   end Test_Uint32_To_Octet4;
    
-   procedure Test_Convert_Uint64 (T : in out Test_Cases.Test_Case'Class) is
+   procedure Test_Uint64_To_Octet8 (T : in out Test_Cases.Test_Case'Class) is
       u64, u64t : uint64;
       o8 : Octets_8;
    begin
@@ -54,9 +61,9 @@ package body types_tests is
       u64t := octets_to_uint64 (o8);
       
       Assert (u64 = u64t, "Converting uint64 to octets and back does not work");
-   end Test_Convert_Uint64;
+   end Test_Uint64_To_Octet8;
    
-   procedure Test_Convert_Octet2 (T : in out Test_Cases.Test_Case'Class) is
+   procedure Test_Octet2_To_Uint16 (T : in out Test_Cases.Test_Case'Class) is
       o2, o2t : Octets_2;
       u16 : uint16;
    begin
@@ -67,9 +74,9 @@ package body types_tests is
       
       Assert (o2 (0) = o2t (0) and o2 (1) = o2t (1), 
               "Converting Octets_2 to uint16 and back does not work");
-   end Test_Convert_Octet2;
+   end Test_Octet2_To_Uint16;
    
-   procedure Test_Convert_Octet4 (T : in out Test_Cases.Test_Case'Class) is
+   procedure Test_Octet4_To_Uint32 (T : in out Test_Cases.Test_Case'Class) is
       o4, o4t : Octets_4;
       u32 : uint32;
    begin
@@ -83,9 +90,9 @@ package body types_tests is
       Assert (o4 (0) = o4t (0) and o4 (1) = o4t (1) and 
               o4 (2) = o4t (2) and o4 (3) = o4t (3), 
               "Converting Octets_4 to uint32 and back does not work");
-   end Test_Convert_Octet4;
+   end Test_Octet4_To_Uint32;
    
-   procedure Test_Convert_Octet8 (T : in out Test_Cases.Test_Case'Class) is
+   procedure Test_Octet8_To_Uint64 (T : in out Test_Cases.Test_Case'Class) is
       o8, o8t : Octets_8;
       u64 : uint64;
    begin
@@ -105,6 +112,57 @@ package body types_tests is
               o8 (4) = o8t (4) and o8 (5) = o8t (5) and
               o8 (6) = o8t (6) and o8 (7) = o8t (7), 
               "Converting Octets_4 to uint64 and back does not work");
-   end Test_Convert_Octet8;
+   end Test_Octet8_To_Uint64;
    
+   procedure Test_Uint64_To_Float64 (T : in out Test_Cases.Test_Case'Class) is
+      u64, u64t : uint64;
+      f64 : float64;
+   begin
+      u64 := 1234567;
+      f64 := uint64_to_float64 (u64);
+      u64t := float64_to_uint64 (f64);
+      Assert (u64 = u64t, "Converting uint64 to float64 and back doe not work");
+   end Test_Uint64_To_Float64;
+   
+   procedure Test_Float64_To_Uint64 (T : in out Test_Cases.Test_Case'Class) is
+      f64, f64t : float64;
+      u64 : uint64;
+   begin
+      f64 := 3.004972;
+      u64 := float64_to_uint64 (f64);
+      f64t := uint64_to_float64 (u64);
+      
+      Assert (f64 = f64t, "Converting float64 to uint64 and back doe not work");
+   end Test_Float64_To_Uint64;
+   
+   procedure Test_Byte_Order_16 (T : in out Test_Cases.Test_Case'Class) is
+      u16i, u16m, u16o : uint16;
+   begin
+      u16i := 45298;
+      u16m := hton16 (u16i);
+      u16o := ntoh16 (u16m);
+      
+      Assert (u16i = u16o, "hton16 and ntoh16 do not work.");
+   end Test_Byte_Order_16;
+   
+   procedure Test_Byte_Order_32 (T : in out Test_Cases.Test_Case'Class) is
+      u32i, u32m, u32o : uint32;
+   begin
+      u32i := 3568298;
+      u32m := hton32 (u32i);
+      u32o := ntoh32 (u32m);
+      
+      Assert (u32i = u32o, "hton32 and ntoh32 do not work.");
+   end Test_Byte_Order_32;
+   
+   procedure Test_Byte_Order_64 (T : in out Test_Cases.Test_Case'Class) is
+      u64i, u64m, u64o : uint64;
+   begin
+      u64i := 6568298;
+      u64m := hton64 (u64i);
+      u64o := ntoh64 (u64m);
+      
+      Assert (u64i = u64o, "hton64 and ntoh64 do not work.");
+   end Test_Byte_Order_64;
+      
 end types_tests;
--- external/harness/src/tcp_suite/types_tests.ads
+++ external/harness/src/tcp_suite/types_tests.ads
@@ -13,12 +13,19 @@ package types_tests is
   -- Provide name identifying the test case
 
   -- Test Routines:
-   procedure Test_Convert_Uint16 (T : in out Test_Cases.Test_Case'Class);
-   procedure Test_Convert_Uint32 (T : in out Test_Cases.Test_Case'Class);
-   procedure Test_Convert_Uint64 (T : in out Test_Cases.Test_Case'Class);
+   procedure Test_Uint16_To_Octet2 (T : in out Test_Cases.Test_Case'Class);
+   procedure Test_Uint32_To_Octet4 (T : in out Test_Cases.Test_Case'Class);
+   procedure Test_Uint64_To_Octet8 (T : in out Test_Cases.Test_Case'Class);
    
-   procedure Test_Convert_Octet2 (T : in out Test_Cases.Test_Case'Class);
-   procedure Test_Convert_Octet4 (T : in out Test_Cases.Test_Case'Class);
-   procedure Test_Convert_Octet8 (T : in out Test_Cases.Test_Case'Class);
+   procedure Test_Octet2_To_Uint16 (T : in out Test_Cases.Test_Case'Class);
+   procedure Test_Octet4_To_Uint32 (T : in out Test_Cases.Test_Case'Class);
+   procedure Test_Octet8_To_Uint64 (T : in out Test_Cases.Test_Case'Class);
+   
+   procedure Test_Uint64_To_Float64 (T : in out Test_Cases.Test_Case'Class);
+   procedure Test_Float64_To_Uint64 (T : in out Test_Cases.Test_Case'Class);
+   
+   procedure Test_Byte_Order_16 (T : in out Test_Cases.Test_Case'Class);
+   procedure Test_Byte_Order_32 (T : in out Test_Cases.Test_Case'Class);
+   procedure Test_Byte_Order_64 (T : in out Test_Cases.Test_Case'Class);
 
 end types_tests;

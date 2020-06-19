9f10f7aef78cfb6c4e9527cc700c0f0ce29842a6
Henrik Classen <henrik.classen@web.de>
2020-06-19 17:21:12 +0200

    changed old test suite accordingly to new suites

--- external/harness/src/tcp_suite/tcp_suite.adb
+++ external/harness/src/tcp_suite/tcp_suite.adb
@@ -1,9 +1,7 @@
 with tcp_tests;
-with types_tests;
-with buffer_tests;
-
 
 package body tcp_suite is
+
    use AUnit.Test_Suites;
 
    --  Statically allocate test suite:
@@ -11,14 +9,11 @@ package body tcp_suite is
 
    --  Statically allocate test cases:
    Test_1 : aliased tcp_tests.tcp_test;
-   Test_2 : aliased types_tests.types_test;
-   Test_3 : aliased buffer_tests.buffer_test;
 
    function Suite return Access_Test_Suite is
    begin
       Add_Test (Result'Access, Test_1'Access);
-      Add_Test (Result'Access, Test_2'Access);
-      Add_Test (Result'Access, Test_3'Access);
       return Result'Access;
    end Suite;
+
 end tcp_suite;
--- external/harness/src/tcp_suite/tcp_suite.ads
+++ external/harness/src/tcp_suite/tcp_suite.ads
@@ -1,6 +1,7 @@
 with AUnit.Test_Suites;
 
-
 package tcp_suite is
+
    function Suite return AUnit.Test_Suites.Access_Test_Suite;
+
 end tcp_suite;

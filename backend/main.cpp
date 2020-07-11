076cf256817e78e7f2dd3318dc36ecbb8067290e
Henrik Claßen <henrik.classen@web.de>
2020-07-11 17:07:35 +0200

    cab provision init

--- backend/main.cpp
+++ backend/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <exception>
 
-#include "application.hpp"
+// #include "application.hpp"
+
+#include "cab_provision.hpp"
 
 int main(int argc, char** argv)
 {
@@ -9,18 +11,20 @@ int main(int argc, char** argv)
 
     int return_value = 0;
 
-    try
-    {
-        application application;
-        return_value = application.run(argc, argv);
-    }
-    catch(std::exception& e)
-    {
-        std::cerr << "Critital error\n" << e.what() << std::endl;
-        std::cerr << "Program terminated" << std::endl;
+    // try
+    // {
+    //     application application;
+    //     return_value = application.run(argc, argv);
+    // }
+    // catch(std::exception& e)
+    // {
+    //     std::cerr << "Critital error\n" << e.what() << std::endl;
+    //     std::cerr << "Program terminated" << std::endl;
 
-        return_value = -1;
-    }
+    //     return_value = -1;
+    // }
+
+    cab_provision(node_id::D, node_id::P3);
 
     return return_value;
 }

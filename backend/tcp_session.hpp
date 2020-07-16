0272839d720d5c2b3dbad23bddb308c33f55413a
Justus Flerlage <justus@sutsuj.com>
2020-07-16 10:58:58 +0200

    Added move and copy constructors and overloadings

--- backend/tcp_session.hpp
+++ backend/tcp_session.hpp
@@ -9,6 +9,10 @@ class tcp_session : public std::enable_shared_from_this<T>
 {
     public:
         tcp_session(application& application);
+        tcp_session(tcp_session<T>&) = delete;
+        tcp_session(tcp_session<T>&&) = default;
+        tcp_session<T>& operator=(const tcp_session<T>&) = delete;
+        tcp_session<T>& operator=(tcp_session<T>&&) = default;
 
         virtual ~tcp_session() = default;
 

109111b7d737444b45e8e8ee241e53d1f33969ad
zsejke <mzsejke@yahoo.com>
2020-07-16 11:28:58 +0200

    changes

--- external/controller/src/collision_detection/collision_detection.adb
+++ external/controller/src/collision_detection/collision_detection.adb
@@ -7,7 +7,7 @@ with Ada.Float_Text_IO;
 package body Collision_Detection is
 
    function Main(Distance: Messages.Distance_Sensor_Array) return Messages.Velocity_Message is
-      threshold: float64 := 500.0;
+      threshold: float64 := 600.0;
    begin
       -- Object in front of car
 --              Ada.Float_Text_IO.Put(FLoat(distance(0)),5,3,0);
@@ -23,7 +23,7 @@ package body Collision_Detection is
       case car_state is
          when Forward =>
             --ada.Text_IO.Put_Line("forward");
-            if distance(0) < 700.0 or distance(1) < threshold or
+            if distance(0) < 1500.0 or distance(1) < threshold or
                distance(2) < threshold then
                ls := -10.0;
                rs :=  10.0;
@@ -40,7 +40,7 @@ package body Collision_Detection is
                rs := 4.0;
             end if;
             if Distance(1) < threshold then
-               ls := 1.0;
+               ls := -1.0;
                rs := 4.0;
             end if;
 

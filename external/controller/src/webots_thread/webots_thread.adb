7c64526c620f3da5343cf08aa27d3e1ee0eb3b84
zsejke <mzsejke@yahoo.com>
2020-07-17 19:45:28 +0200

    starting one cab from command line: ./main {port_number}

--- external/controller/src/webots_thread/webots_thread.adb
+++ external/controller/src/webots_thread/webots_thread.adb
@@ -1,14 +1,24 @@
+with Ada.Command_line; use Ada.Command_Line;
+with Ada.Text_IO; use Ada.Text_IO;
+with ada.Integer_Text_IO;
+
+
 package body Webots_Thread is
 
    procedure Main is
 
    begin
 
-      Webots_Channel := Tcp_Client.Connect (Webots_Client, 6666, Webots_Address);
+   ada.Text_IO.Put(" COmmmand ");
+   ada.Integer_Text_IO.put(Argument_Count);
+   ada.Text_IO.Put(Argument(1));
+
+      Webots_Channel := Tcp_Client.Connect (Webots_Client, Integer(Argument(1)), Webots_Address);
 
       while true loop
          Tcp_Client.Read_Packet (Webots_Channel, Webots_Vector_Buffer, Webots_Mailbox);
       end loop;
+
 
    end Main;
 

18cebe2430a604ce41b123465f4cdcf6552ca830
zsejke <mzsejke@yahoo.com>
2020-07-17 19:01:50 +0200

    Revert "multiple cabs started"
    This reverts commit 991c1451879458a8df8350cdcd0a429418666697.

--- external/controller/src/path/path_following.adb
+++ external/controller/src/path/path_following.adb
@@ -70,7 +70,7 @@ package body Path_Following is
       end loop;
 
       bottomPoint := (bottomPoint + bottomPoint1) / 2;
- --     Put_Line(Integer'Image(bottomPoint));
+      Put_Line(Integer'Image(bottomPoint));
       return bottomPoint;
 
    end Find_Line;
@@ -96,8 +96,8 @@ package body Path_Following is
          end if;
       end if;
 
---        Put_Line (d_sensor(3)'Image & ", " & d_sensor(6)'Image);
---        Put_Line (V_turn'Image);
+      Put_Line (d_sensor(3)'Image & ", " & d_sensor(6)'Image);
+      Put_Line (V_turn'Image);
       --turn right
 
       if V_turn > 0.0 then
--- external/controller/src/webots_thread/webots_thread.ads
+++ external/controller/src/webots_thread/webots_thread.ads
@@ -11,14 +11,10 @@ package Webots_Thread is
    -- Webots thread variables
    Webots_Client  : Socket_Type; -- stores the socket for the webots controller
    Webots_Channel : Stream_Access; -- socket I/O interface
-   Webots_Channel2 : Stream_Access; -- socket I/O interface
    Webots_Address : Sock_Addr_Type; -- stores the server address
    Webots_Cmd : Types.Communication_Packet; -- command to send over socket
    Webots_Vector_Buffer : Byte_buffer.Buffer;
    Webots_Mailbox : Mailbox.Mailbox (Size => 5);
-   Webots_Vector_Buffer2 : Byte_buffer.Buffer;
-   Webots_Mailbox2 : Mailbox.Mailbox (Size => 5);
-
 
    procedure Main;
 
